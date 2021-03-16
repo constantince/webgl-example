8d0671ce7a64069d5efc088a604bb8907fcb7309
陈焱 <alberteinstein007@126.com>
2021-03-16 16:15:43 +0800

    feat: move the triangle animation

--- objs/obj_1.js
+++ objs/obj_1.js
@@ -2,8 +2,9 @@ const vertex = `
     attribute vec4 a_Position;
     attribute vec4 a_Color;
     varying vec4 v_Color;
+    uniform mat4 u_ViewMatrix;
     void main() {
-        gl_Position = a_Position;
+        gl_Position = u_ViewMatrix * a_Position;
         v_Color = a_Color;
     }
 `;
@@ -76,18 +77,43 @@ function initBuffer(gl, program) {
     return 4;
 }
 
+function initMatrix(gl, program, angle, x) {
+    const M = new Matrix4();
+    // console.log("angle is", angle);
+    // M.setTranslate(x, 0, 0).rotate(angle, 0, 0, 1);
+    M.setRotate(angle, 0, 0, 1);
+    const original = gl.getUniformLocation(program, "u_ViewMatrix");
+    gl.uniformMatrix4fv(original, false, M.elements)
+}
+
 function main() {
     const canvas = document.getElementById("cube");
     const gl = canvas.getContext("webgl");
-
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
     const program = initShader(gl, vertex, fragment);
-
     const number = initBuffer(gl, program);
+    let va = 1, vx = 0.01, original_angle = 0, original_x = 0;
 
-    gl.clear(gl.COLOR_BUFFER_BIT);
+   
 
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, number)
+    function move() {
+        original_angle = original_angle - va;
+        original_x = original_x + vx;
+        initMatrix(gl, program, original_angle, original_x);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.drawArrays(gl.TRIANGLE_STRIP, 0, number);
+        requestAnimationFrame(move);
+    }
+
+    move();
+    
+
+    
+    
+    
+
+    // move();
 }
+
+
 
