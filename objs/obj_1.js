61f5ba97a00b7e70a3abb6f61b3230bb85ee3e7b
陈焱 <alberteinstein007@126.com>
2021-03-19 16:34:26 +0800

    feat: add new rotate cube with img texture

--- objs/obj_1.js
+++ objs/obj_1.js
@@ -23,11 +23,7 @@ var FSHADER_SOURCE =
   'varying vec4 v_Color;\n' +
   'uniform bool u_Special;\n' +
   'void main() {\n' +
-  '  if (v_Color.r == 0.0 && v_Color.g == 0.0 && v_Color.b == 0.0) {\n' + //  Draw in red if mouse is pressed
-  '     gl_FragColor = texture2D(u_Sampler, v_TexCoord);\n' +
-  '  } else {\n' +
-  '    gl_FragColor = v_Color;\n' +
-  '  }\n' +
+  '   gl_FragColor = texture2D(u_Sampler, v_TexCoord);\n' +
   '}\n';
 
 function main() {
@@ -48,8 +44,8 @@ function main() {
   }
 
   // Set the vertex information
-  var n = initVertexBuffers(gl);
-  if (n < 0) {
+  
+  if (32 < 0) {
     console.log('Failed to set the vertex information');
     return;
   }
@@ -75,19 +71,44 @@ function main() {
   initEventHandlers(canvas, currentAngle);
 
   // Set texture
-  if (!initTextures(gl)) {
-    console.log('Failed to intialize the texture.');
-    return;
-  }
+  // if (!initTextures(gl)) {
+  //   console.log('Failed to intialize the texture.');
+  //   return;
+  // }
 
-  var tick = function() {   // Start drawing
-    draw(gl, n, viewProjMatrix, u_MvpMatrix, currentAngle);
-    requestAnimationFrame(tick, canvas);
-  };
-  tick();
+  // initIndex(gl, [ 0, 1, 2,   0, 2, 3]);
+  // draw(gl, n, viewProjMatrix, u_MvpMatrix, currentAngle);
+  // initIndex(gl, [ 4, 5, 6,   4, 6, 7]);
+  // draw(gl, n, viewProjMatrix, u_MvpMatrix, currentAngle);
+  // initIndex(gl, [ 8, 9,10,   8,10,11]);
+  // draw(gl, n, viewProjMatrix, u_MvpMatrix, currentAngle);
+  // initIndex(gl, [ 12,13,14,  12,14,15]);
+  // draw(gl, n, viewProjMatrix, u_MvpMatrix, currentAngle);
+  // initIndex(gl, [ 16,17,18,  16,18,19]);
+  // draw(gl, n, viewProjMatrix, u_MvpMatrix, currentAngle);
+  // initIndex(gl, [ 20,21,22,  20,22,23 ]);
+  // draw(gl, n, viewProjMatrix, u_MvpMatrix, currentAngle);
+  initVertexBuffers(gl, viewProjMatrix, u_MvpMatrix, currentAngle);
+  // var tick = function() {   // Start drawing
+  //   initVertexBuffers(gl, viewProjMatrix, u_MvpMatrix, currentAngle);
+  //   // draw(gl, 6, viewProjMatrix, u_MvpMatrix, currentAngle);
+  //   // requestAnimationFrame(tick);
+
+  // };
+  // tick();
 }
 
-function initVertexBuffers(gl) {
+//   // Indices of the vertices
+//   var indices = new Uint8Array([
+//     0, 1, 2,   0, 2, 3,    // front
+//     4, 5, 6,   4, 6, 7,    // right
+//     8, 9,10,   8,10,11,    // up
+//    12,13,14,  12,14,15,    // left
+//    16,17,18,  16,18,19,    // down
+//    20,21,22,  20,22,23     // back
+//  ]);
+
+function initVertexBuffers(gl, viewProjMatrix, u_MvpMatrix, currentAngle) {
   // Create a cube
   //    v6----- v5
   //   /|      /|
@@ -123,7 +144,7 @@ function initVertexBuffers(gl) {
       0.0, 0.0,   1.0, 0.0,   1.0, 1.0,   0.0, 1.0     // v4-v7-v6-v5 back
   ]);
 
-  // Indices of the vertices
+  // // Indices of the vertices
   var indices = new Uint8Array([
      0, 1, 2,   0, 2, 3,    // front
      4, 5, 6,   4, 6, 7,    // right
@@ -133,25 +154,55 @@ function initVertexBuffers(gl) {
     20,21,22,  20,22,23     // back
   ]);
 
-  // Create a buffer object
-  var indexBuffer = gl.createBuffer();
-  if (!indexBuffer) {
-    return -1;
-  }
-
   // Write vertex information to buffer object
   if (!initArrayBuffer(gl, vertices, 3, gl.FLOAT, 'a_Position')) return -1; // Vertex coordinates
   if (!initArrayBuffer(gl, texCoords, 2, gl.FLOAT, 'a_TexCoord')) return -1;// Texture coordinates
-  if (!initArrayBuffer(gl, color, 3, gl.FLOAT, 'a_Color')) return -1;// Texture coordinates
+  // if (!initArrayBuffer(gl, color, 3, gl.FLOAT, 'a_Color')) return -1;// Texture coordinates
 
   // Unbind the buffer object
   gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
-  // Write the indices to the buffer object
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+
+  // var drawed = false;
+  var tick = function() {   // Start drawing
+    initIndex('./brick.jpg', gl, [ 0, 1, 2,   0, 2, 3], viewProjMatrix, u_MvpMatrix, currentAngle);
+    initIndex('./sky.jpg', gl, [ 4, 5, 6,   4, 6, 7], viewProjMatrix, u_MvpMatrix, currentAngle);
+    initIndex("./js.jpg",gl, [ 8, 9,10,   8,10,11], viewProjMatrix, u_MvpMatrix, currentAngle);
+    initIndex("./python.png", gl, [ 12,13,14,  12,14,15], viewProjMatrix, u_MvpMatrix, currentAngle);
+    initIndex("./brick1.png", gl, [ 16,17,18,  16,18,19], viewProjMatrix, u_MvpMatrix, currentAngle);
+    initIndex("./css.jpeg", gl, [ 20,21,22,  20,22,23 ], viewProjMatrix, u_MvpMatrix, currentAngle);
+    
+    // draw(gl, 6, viewProjMatrix, u_MvpMatrix, currentAngle);
+    // setTimeout(function() {
+      requestAnimationFrame(tick);
+    // }, 2000)
+   
+
+  };
+  tick();
+  
+
+
+  
 
   return indices.length;
+}
+
+var shadow = {};
+var g_imgs = [];
+function initIndex(src, gl, ind, viewProjMatrix, u_MvpMatrix, currentAngle) {
+    var indexBuffer = gl.createBuffer();
+    if (!indexBuffer) {
+      return -1;
+    }
+  // Write the indices to the buffer object
+  // Create a buffer object
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(ind), gl.STATIC_DRAW); 
+    initTextures(src, gl, viewProjMatrix, u_MvpMatrix, currentAngle, indexBuffer);
+  
+  
+
 }
 
 function initEventHandlers(canvas, currentAngle) {
@@ -192,8 +243,9 @@ function draw(gl, n, viewProjMatrix, u_MvpMatrix, currentAngle) {
   g_MvpMatrix.rotate(currentAngle[1], 0.0, 1.0, 0.0); // Rotation around y-axis
   gl.uniformMatrix4fv(u_MvpMatrix, false, g_MvpMatrix.elements);
 
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);     // Clear buffers
+  
   gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);   // Draw the cube
+  // gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);     // Clear buffers
 }
 
 function initArrayBuffer(gl, data, num, type, attribute) {
@@ -219,7 +271,11 @@ function initArrayBuffer(gl, data, num, type, attribute) {
   return true;
 }
 
-function initTextures(gl) {
+function initTextures(src, gl, viewProjMatrix, u_MvpMatrix, currentAngle, b) {
+
+
+  
+
   // Create a texture object
   var texture = gl.createTexture();
   if (!texture) {
@@ -234,21 +290,28 @@ function initTextures(gl) {
     return false;
   }
 
-  // Create the image object
-  var image = new Image();
-  if (!image) {
-    console.log('Failed to create the image object');
-    return false;
-  }
-  // Register the event handler to be called when image loading is completed
-  image.onload = function(){ loadTexture(gl, texture, u_Sampler, image); };
-  // Tell the browser to load an Image
-  image.src = './brick.jpg';
+  if(shadow[src]) {
+    loadTexture(gl, texture, u_Sampler, shadow[src], viewProjMatrix, u_MvpMatrix, currentAngle, b);
+  } else {
+    // Create the image object
+    var image = new Image();
+    if (!image) {
+      console.log('Failed to create the image object');
+      return false;
+    }
+    // Register the event handler to be called when image loading is completed
+    image.onload = function(){ loadTexture(gl, texture, u_Sampler, image, viewProjMatrix, u_MvpMatrix, currentAngle, b); };
+    // Tell the browser to load an Image
+    image.src = src;
+    shadow[src] = image;
 
-  return true;
+    return true;
+  }
+
+  
 }
 
-function loadTexture(gl, texture, u_Sampler, image) {
+function loadTexture(gl, texture, u_Sampler, image, viewProjMatrix, u_MvpMatrix, currentAngle, b) {
   gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);  // Flip the image Y coordinate
   // Activate texture unit0
   gl.activeTexture(gl.TEXTURE0);
@@ -264,4 +327,8 @@ function loadTexture(gl, texture, u_Sampler, image) {
 
   // Pass the texure unit 0 to u_Sampler
   gl.uniform1i(u_Sampler, 0);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, b);
+
+  console.log("drawed.");
+  draw(gl, 6, viewProjMatrix, u_MvpMatrix, currentAngle);
 }
